ba0114be383d79340c35b344290cd71987237901
jackdo1012 <dominhtrijack@gmail.com>
2022-01-26 16:36:31 +0700

    add auth to delete post

--- backend/src/services/image.services.ts
+++ backend/src/services/image.services.ts
@@ -1,23 +1,39 @@
 import { getGfs } from "../config/db.config";
 
+interface File extends Express.Multer.File {
+    _id: string;
+}
+
 export const deleteImgs: (
-    imgsName: string[]
+    imgsName: string[],
 ) => Promise<{ success: boolean; message?: string }> = async (imgsName) => {
     try {
-        const promiseList: Promise<void>[] = [];
+        const promiseList: any[] = [];
         imgsName.forEach((imgName) => {
             promiseList.push(
                 new Promise((resolve, reject) => {
-                    getGfs().delete({ filename: imgName }, (err: any) => {
-                        if (err) return reject();
-                        resolve();
-                    });
-                })
+                    getGfs()
+                        .find({ filename: imgName })
+                        .toArray((err: Error, files: File[]) => {
+                            if (err) {
+                                reject("Error while deleting image");
+                            } else {
+                                if (!files || files.length <= 0) {
+                                    reject("Image not found");
+                                }
+                                console.log(files[0]);
+                                const postId = files[0]["_id"];
+                                getGfs().delete(postId);
+                                resolve("Img deleted");
+                            }
+                        });
+                }),
             );
         });
         await Promise.all(promiseList);
         return { success: true };
     } catch (err) {
+        console.log(err);
         return { success: false, message: err.message };
     }
 };
